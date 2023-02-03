9305010afd1f7a91f6400e7f62bb4419852dfdb4
Rafael César Rocha Monteiro <108206396+RafaRock1@users.noreply.github.com>
2023-02-03 15:02:56 -0300

    urna

---
+++ urna_js/etapas.js
@@ -0,0 +1,263 @@
+let etapas = [
+    {
+        titulo: 'Deputado_Estadual',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '10369',
+                nome: '  RODRIGUES DE OLIVEIRA',
+                partido: 'REPUBLICANOS',
+                vice:'-----------------',
+                fotos: [
+                    {url:'deputado_estadual/10369.jpg', legenda: 'Deputado Estadual'}
+                ]
+            },
+            {
+                numero: '33444',
+                nome: 'ADRIANO BARBOSA GOMES DA SILVA',
+                partido: 'PMB',
+                vice:'-----------------',
+                fotos: [
+                    {url:'deputado_estadual/33444.jpg', legenda: 'Deputado Estadual'}
+                ]
+            },
+            {
+                numero: '22222',
+                nome: 'ALCIDES FERNANDES DA SILVA',
+                partido: 'PL',
+                vice:'-----------------',
+                fotos: [
+                    {url:'deputado_estadual/22222.jpg', legenda: 'Deputado Estadual'}
+                ]
+            },
+            {
+                numero: '12789',
+                nome: 'ALESSANDRA AIRES SABINO VIEIRA',
+                partido: 'PDT',
+                vice:'-----------------',
+                fotos: [
+                    {url:'deputado_estadual/12789.jpg', legenda: 'Deputado Estadual'}
+                ]
+            },
+            {
+                numero: '13777',
+                nome: 'ANTONIO VALDENIZO DA COSTA',
+                partido: 'PT',
+                vice:'-----------------',
+                fotos: [
+                    {url:'deputado_estadual/13777.jpg', legenda: 'Deputado Estadual'}
+                ]
+            },
+        ]
+    },
+
+    {
+        titulo: 'Deputado_Federal',
+        numeros: 4,
+        candidatos: [
+            {
+                numero: '1516',
+                nome: 'ABRAÃO LUCAS',
+                partido: 'MDB',
+                vice:'-----------------',
+                fotos: [
+                    {url:'deputado_federal/1516.jpg', legenda: 'Deputado Federal'}
+                ]
+            },
+            {
+                numero: '1022',
+                nome: 'ADAIL CARNEIRO',
+                partido: 'REPUBLICANOS',
+                vice:'-----------------',
+                fotos: [
+                    {url:'deputado_federal/1022.jpg', legenda: 'Deputado Federal'}
+                ]
+            },
+            {
+                numero: '5013',
+                nome: 'ADELITA MONTEIRO',
+                partido: 'PSOL',
+                vice:'-----------------',
+                fotos: [
+                    {url:'deputado_federal/5013.jpg', legenda: 'Deputado Federal'}
+                ]
+            },
+            {
+                numero: '2222',
+                nome: 'ANDRÉ FERNANDES',
+                partido: 'PDT',
+                vice:'-----------------',
+                fotos: [
+                    {url:'deputado_federal/2222.jpg', legenda: 'Deputado Estadual'}
+                ]
+            },
+            {
+                numero: '1321',
+                nome: 'CHRISTIANNE COELHO',
+                partido: 'PT',
+                vice:'-----------------',
+                fotos: [
+                    {url:'deputado_federal/1321.jpg', legenda: 'Deputado Federal'}
+                ]
+            },
+        ]
+    },
+
+    {
+        titulo: 'Senador',
+        numeros: 3,
+        candidatos: [
+            {
+                numero: '131',
+                nome: 'CAMILO SANTANA',
+                partido: 'PT',
+                vice:'-----------------',
+                fotos: [
+                    {url:'senador/131.jpg', legenda: 'Senador'}
+                ]
+            },
+            {
+                numero: '160',
+                nome: 'CARLOS SILVA',
+                partido: 'PSTU',
+                vice:'-----------------',
+                fotos: [
+                    {url:'senador/160.jpg', legenda: 'Senador'}
+                ]
+            },
+            {
+                numero: '700',
+                nome: 'KAMILA CARDOSO',
+                partido: 'AVANTE',
+                vice:'-----------------',
+                fotos: [
+                    {url:'senador/700.jpg', legenda: 'Senador'}
+                ]
+            },
+            {
+                numero: '555',
+                nome: 'ÉRIKA AMORIM',
+                partido: 'PSD',
+                vice:'-----------------',
+                fotos: [
+                    {url:'senador/555.jpg', legenda: 'Senador'}
+                ]
+            },
+        ]
+    },
+
+    {
+        titulo: 'Governador',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '44',
+                nome: 'CAPITÃO WAGNER',
+                partido: 'UNIÃO BRASIL',
+                vice:'RAIMUNDO MATOS',
+                fotos: [
+                    {url:'governador/44.jpg', legenda: 'Governador'},
+                    {url:'governador/vice44.jpeg', legenda: 'Vice-Governador'}
+                ]
+            },
+            {
+                numero: '21',
+                nome: 'CHICO MALTA',
+                partido: 'PCB',
+                vice:'NAURI ARAÚJO',
+                fotos: [
+                    {url:'governador/21.jpg', legenda: 'Governador'},
+                    {url:'governador/vice21.jpg', legenda: 'Vice-Governador'}
+                ]
+            },
+            {
+                numero: '13',
+                nome: 'ELMANO DE FREITAS',
+                partido: 'PT',
+                vice:'JADE ROMERO',
+                fotos: [
+                    {url:'governador/13.jpeg', legenda: 'Governador'},
+                    {url:'governador/vice13.jpeg', legenda: 'Vice-Governador'}
+                ]
+            },
+            {
+                numero: '12',
+                nome: 'ROBERTO CLÁUDIO',
+                partido: 'PSD',
+                vice:'DOMINGOS FILHO',
+                fotos: [
+                    {url:'governador/12.jpg', legenda: 'Governador'},
+                    {url:'governador/vice12.jpg', legenda: 'Vice-Governador'}
+                ]
+            },
+            {
+                numero: '80',
+                nome: 'SERLEY LEAL',
+                partido: 'UP',
+                vice:'FRANCISCO BITA TAPEBA',
+                fotos: [
+                    {url:'governador/80.jpg', legenda: 'Governador'},
+                    {url:'governador/vice80.jpg', legenda: 'Vice-Governador'}
+                ]
+            },
+        ]
+    },
+
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos:[
+            {
+                numero:'22',
+                nome:'JAIR BOLSONARO',
+                partido:'PL',
+                vice:'WALTER BRAGA',
+                fotos:[
+                    {url:'presidente/bolsonaro.jpg', legenda: 'Presidente'},
+                    {url:'presidente/walter.jpeg', legenda: 'Vice', small: true}
+                ]
+            },
+            {
+                numero:'13',
+                nome:'LULA',
+                partido:'PT',
+                vice:'GERALDO ALCKMIN',
+                fotos:[
+                    {url:'presidente/ladrao.jpg', legenda: 'Presidente'},
+                    {url:'presidente/vice-ladrao.jpg', legenda: 'Vice', small: true}
+                ]
+            },
+            {
+                numero:'44',
+                nome:'SORAYA THRONICKE',
+                partido:'UNIÃO',
+                vice:'MARCOS CINTRA',
+                fotos:[
+                    {url:'presidente/44.jpg', legenda: 'Presidente'},
+                    {url:'presidente/vice44.jpg', legenda: 'Vice', small: true}
+                ]
+            },
+            {
+                numero:'14',
+                nome:'PADRE KELMON',
+                partido:'PTB',
+                vice:'LUIZ GAMONAL',
+                fotos:[
+                    {url:'presidente/14.jpg', legenda: 'Presidente'},
+                    {url:'presidente/vice14.jpg', legenda: 'Vice', small: true}
+                ]
+            },
+            {
+                numero:'12',
+                nome:'CIRO GOMES',
+                partido:'PDT',
+                vice:'ANA PAULA',
+                fotos:[
+                    {url:'presidente/12.jpg', legenda: 'Presidente'},
+                    {url:'presidente/vice12.jpeg', legenda: 'Vice', small: true}
+                ]
+            },
+
+        ]
+    }
+];
